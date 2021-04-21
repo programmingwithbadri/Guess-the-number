bd0a82499b68bc166f7f92bf0340c626f42f4bdd
Badri Narayanan <programmingwithbadri@outlook.com>
2021-04-21 22:58:40 +0530

    Check the winCondition using useEffect and navigate to GameOverScreen

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './src/components/Header';
-import GameScreen from './src/screens/GameScreen';
 import StartGameScreen from './src/screens/StartGameScreen';
+import GameScreen from './src/screens/GameScreen';
+import GameOverScreen from './src/screens/GameOverScreen';
 
 const App = () => {
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
   };
 
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
+  };
+
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  if (userNumber) {
+  if (userNumber && guessRounds <= 0) {
     content = (
-      <GameScreen userChoice={userNumber} />
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen />
     );
   }
 
---
+++ src/screens/GameOverScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const GameOverScreen = () => {
+    return (
+        <View>
+            <Text>The Game is over</Text>
+        </View>
+    )
+}
+
+export default GameOverScreen
+
+const styles = StyleSheet.create({})
