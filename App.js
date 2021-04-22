e76b61ffb9d123e4699bdf9189c9656fd58cbe61
Badri Narayanan <programmingwithbadri@outlook.com>
2021-04-22 23:16:10 +0530

    Add the custom fonts to the app

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import React, { useState } from 'react';
+import AppLoading from 'expo-app-loading';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
 import Header from './src/components/Header';
 import StartGameScreen from './src/screens/StartGameScreen';
 import GameScreen from './src/screens/GameScreen';
 import GameOverScreen from './src/screens/GameOverScreen';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
 const App = () => {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
